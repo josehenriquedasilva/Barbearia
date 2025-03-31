8e5f94aed3412974a17af290b0ee64dae64af836
josehenriquedasilva <josehenrique60t@gmail.com>
2025-03-31 17:24:22 -0300

    Atualização

--- interações.js
+++ interações.js
@@ -1,17 +1,29 @@
 const abrirMenu = document.querySelector('#abrir');
-const menu = document.querySelector('#menu2');
+const menu2 = document.querySelector('#menu2');
 const fecharMenu = document.querySelector('#fechar');
+const menu = document.querySelector('.menuFixo');
 
 abrirMenu.addEventListener('click', () => {
-    if (menu.classList.contains('ativo')) {
-        menu.classList.remove('ativo');
+    if (menu2.classList.contains('ativo')) {
+        menu2.classList.remove('ativo');
     } else {
-        menu.classList.add('ativo');
-        menu.style.transform = 'translateX(0%)';
+        menu2.classList.add('ativo');
+        menu2.style.transform = 'translateX(0%)';
     }
 });
 
 fecharMenu.addEventListener('click', () => {
-    menu.style.transform = 'translateX(100%)';
-    menu.classList.remove('ativo');
+    menu2.style.transform = 'translateX(100%)';
+    menu2.classList.remove('ativo');
 });
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 1) {
+        menu.classList.add('menuFixo-ativo')
+        menu.style.visibility = 'visible';
+    } else {
+        menu.style.visibility = 'hidden'
+        menu.classList.remove('menuFixo-ativo')
+    }
+});
+
